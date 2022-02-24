2c07b401af38ca19afd17e5e558e4d831080b4a6
eliott mogenet <eliott.mogenet@gmail.com>
2022-02-24 14:01:58 -0300

    created graph with Chartkick

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+p "creating users"
+
+user1 =  User.create(email: "test1@gmail.com", password: "test1@gmail.com")
+user1.save!
+
+user2 =  User.create(email: "test2@gmail.com", password: "test2@gmail.com")
+user2.save!
+
+user3 =  User.create(email: "test3@gmail.com", password: "test3@gmail.com")
+user3.save!
