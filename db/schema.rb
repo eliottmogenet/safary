8b7b28ede982ce1e5a00d229d5977004f491da7d
eliott mogenet <eliott.mogenet@gmail.com>
2022-02-24 21:27:13 -0300

    added discriminator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_225324) do
+ActiveRecord::Schema.define(version: 2022_02_25_001843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_225324) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "discriminator"
   end
 
   create_table "guild_users", force: :cascade do |t|
