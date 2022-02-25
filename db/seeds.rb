98b1fd41356a83d2af9685dd055e39dbc201389e
eliott mogenet <eliott.mogenet@gmail.com>
2022-02-25 12:52:10 -0300

    save before experiment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
+GuildUser.destroy_all
 Guild.destroy_all
+DiscordUser.destroy_all
 User.destroy_all
 
 p "creating users"
@@ -23,8 +25,23 @@ user3.save!
 
 p "creating guilds"
 
-guild1 = user1.guilds.create(id: "1", name: "guil1")
+guild1 = user1.guilds.create(id: 1, name: "guild1")
 guild1.save!
 
 
+p "creating discord_users"
 
+discord_users1 = DiscordUser.create(id: 1, username: "discord_user1")
+discord_users1.save!
+
+
+discord_users2 = DiscordUser.create(id: 2, username: "discord_user2")
+discord_users2.save!
+
+#p "creating guild_users"
+
+#guilduser1 = guild1.guild_users.create(id: 1, join_at: "09/14/2009 00:00", discord_user_id: discord_users1.id)
+#guilduser1.save!
+
+#guilduser2 = guild1.guild_users.create(id: 2, join_at: "09/15/2009 00:00", discord_user_id: discord_users2.id)
+#guilduser2.save!
