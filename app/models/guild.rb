b8f2800bb98446c6ef015cced4e73dfd7f7e33e8
eliott mogenet <eliott.mogenet@gmail.com>
2022-03-07 09:33:03 -0300

    add modal for refresh

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -3,10 +3,5 @@ class Guild < ApplicationRecord
   has_many :guild_comments
   has_many :guild_tactics
   belongs_to :user, optional: true
-
-  after_create :reload_page
-
-  def reload_page
-    redirect_to dashboards_path
-  end
 end
+
