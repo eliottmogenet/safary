acc9ae363f5eb2d52ddd9d3d7ad6245d01bd185c
eliott mogenet <eliott.mogenet@gmail.com>
2022-03-17 16:30:46 -0300

    new settup forest admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
